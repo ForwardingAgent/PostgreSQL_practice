d0631310d796d8f954c76cdfb0a60d6ad0d680cd
ForwardingAgent <nikolaspdk@gmail.com>
2024-01-12 19:00:20 +0300

    new script

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-docker exec pgdb_container bash -c "psql -d $POSTGRES_DB -U $POSTGRES_USER \
--c 'CREATE DATABASE books_store;'"
+docker exec pgdb_container psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'CREATE DATABASE books_store;'
