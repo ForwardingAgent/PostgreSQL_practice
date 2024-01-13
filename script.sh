70631851faf8c5632e45929934afce8149b6f9a7
ForwardingAgent <nikolaspdk@gmail.com>
2024-01-13 09:00:27 +0300

    print  Мой секрет

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 
-echo "$POSTGRES_DB"
+echo "Мой секрет: POSTGRES_DB это $POSTGRES_DB"
 
 # docker exec pgdb_container psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'CREATE DATABASE books_store;'
