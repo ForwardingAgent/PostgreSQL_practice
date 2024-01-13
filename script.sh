0e82c7aeb6012c052a401e0d96e3d8eb4e7ee331
ForwardingAgent <nikolaspdk@gmail.com>
2024-01-13 08:34:29 +0300

    2 variants var

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-docker exec pgdb_container psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'CREATE DATABASE books_store;'
+
+echo $POSTGRES_DB
+echo ${{ secrets.POSTGRES_DB }}
+
+# docker exec pgdb_container psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'CREATE DATABASE books_store;'
