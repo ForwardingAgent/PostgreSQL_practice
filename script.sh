75d70d254a4a9c26645af5df984b6e85569c89cb
ForwardingAgent <nikolaspdk@gmail.com>
2024-01-13 09:02:12 +0300

    create db in script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 
-echo "Мой секрет: POSTGRES_DB это $POSTGRES_DB"
+# echo "Мой секрет: POSTGRES_DB это $POSTGRES_DB"
 
-# docker exec pgdb_container psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'CREATE DATABASE books_store;'
+docker exec pgdb_container psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'CREATE DATABASE books_store;'
